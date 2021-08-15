e0999f6e961fcf721d587816daadbe313769900d
Akshith Yellapragada <ayellapragada@gmail.com>
2021-08-14 20:46:14 -0400

    Add simplecov we aim big here

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    enable_coverage :branch
+  end
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
